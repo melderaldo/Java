bf35a26d20fd18b409733fe9bcd3c9fb0b8aeb93
melderaldo <melina.deraldo@gmail.com>
2018-11-10 19:03:11 -0200

    Add @Primary to make spring chose between two implementation.

--- spring-in-5-steps/src/main/java/com/melina/springin5steps/sortAlgorithm/QuickSortAlgorithm.java
+++ spring-in-5-steps/src/main/java/com/melina/springin5steps/sortAlgorithm/QuickSortAlgorithm.java
@@ -1,5 +1,10 @@
 package com.melina.springin5steps.sortAlgorithm;
 
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Component;
+
+@Component
+@Primary
 public class QuickSortAlgorithm implements SortAlgorithm {
 
     public int[] sort(int [] numbers) {
